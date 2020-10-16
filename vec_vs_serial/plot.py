8de9b857d63a726c5fecfc0d825d7560ee91b39b
anthony <boulmieran@gmail.com>
2020-10-16 10:04:24 +0200

    title, update fig

--- vec_vs_serial/plot.py
+++ vec_vs_serial/plot.py
@@ -1,10 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
-
+title = 'Vector multiplication benchmark it = 10M'
 ser = np.genfromtxt('ser-benchmark.data', delimiter=' ')  
 vec = np.genfromtxt('vec-benchmark.data', delimiter=' ')
 plt.figure()
-plt.title('Vector multiplication benchmark loop = (10M * X FLOP)')
+plt.title(title)
 plt.plot(ser[:, 0], ser[:, 3], marker='^', label='serial')
 plt.plot(vec[:, 0], vec[:, 3], marker='o', label='AVG vectorized')
 plt.xlabel('FLOP per loop iteration')
@@ -14,7 +14,7 @@ plt.legend()
 plt.savefig('time_vec_vs_ser_vector_op.pdf')
 
 plt.figure()
-plt.title('Vector multiplication benchmark it = 10M')
+plt.title(title)
 plt.plot(ser[:, 0], vec[:, 3] / vec[:, 3], marker='^', label='serial')
 
 plt.plot(vec[:, 0], ser[:, 3] / vec[:, 1], marker='o', label='BEST    vectorized')
